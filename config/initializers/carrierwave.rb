c635ed7a968ca22fbb93529abe222eb50f5697b3
johnreaganmoore <johnreaganmoore@gmail.com>
2016-09-19 20:41:16 -0400

    Update env variable usage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,8 +9,9 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => Rails.application.secrets.amazon_s3_access_key_id,
-    :aws_secret_access_key  => Rails.application.secrets.amazon_s3_secret_access_key,
+    :aws_access_key_id      => ENV["amazon_s3_access_key_id"],
+    :aws_secret_access_key  => ENV["amazon_s3_secret_access_key"]
+    # :aws_secret_access_key  => Rails.application.secrets.amazon_s3_secret_access_key,
     # :region                 => 'us-standard' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "gaffer-images"
