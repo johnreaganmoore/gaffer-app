ff99280b1fc291dada826a9d13d9e325752cdf97
johnreaganmoore <johnreaganmoore@gmail.com>
2016-09-24 12:40:44 -0400

    Change geocode configuration to non-ssl settings

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,14 +1,15 @@
 # TODO for deployment to product need to include the google api_key and make https true.
+# TODO do this after getting an ssl cert
 
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
   # lookup: :google,            # name of geocoding service (symbol)
   # language: :en,              # ISO-639 language code
-  use_https: true,           # use HTTPS for lookup requests? (if supported)
+  # use_https: true,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: ENV["google_maps_geocoding"],   # API key for geocoding service
+  # api_key: ENV["google_maps_geocoding"],   # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #keys)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
