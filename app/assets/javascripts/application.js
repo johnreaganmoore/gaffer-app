5441251ba992b77847b0d735a281557c7bc820df
johnreaganmoore <johnreaganmoore@gmail.com>
2016-11-04 11:01:10 -0400

    Add messaging and improve team season card

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,18 +30,28 @@ $( document ).on('turbolinks:load', function() {
   $('ul.tabs').tabs();
 
   $('.dropdown-button').dropdown({
-     inDuration: 300,
-     outDuration: 225,
-     constrain_width: false, // Does not change width of dropdown to that of the activator
-     hover: false, // Activate on hover
-     gutter: 0, // Spacing from edge
-     belowOrigin: false, // Displays dropdown below the button
-     alignment: 'right' // Displays dropdown with edge aligned to the left of button
-   }
- );
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+    }
+  );
 
- $('.collapsible').collapsible({
+  $('.collapsible').collapsible({
     // accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
+  $('.modal-trigger').leanModal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    in_duration: 300, // Transition in duration
+    out_duration: 200, // Transition out duration
+    starting_top: '4%', // Starting top style attribute
+    ending_top: '10%' // Ending top style attribute
+    //ready: function() { alert('Ready'); }, // Callback for Modal open
+    //complete: function() { alert('Closed'); } // Callback for Modal close
   });
 
 
