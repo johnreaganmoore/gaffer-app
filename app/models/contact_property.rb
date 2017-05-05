7ae99d712173f9f2c151d5a50a153d82a29f52b8
johnreaganmoore <johnreaganmoore@gmail.com>
2017-05-05 07:32:26 -0400

    Fix issue of contact properties only showing for new contacts

--- app/models/contact_property.rb
+++ app/models/contact_property.rb
@@ -1,4 +1,13 @@
 class ContactProperty < ApplicationRecord
   belongs_to :org
   has_many :contact_values
+
+  after_create :set_defaults_on_existing_contacts
+
+  def set_defaults_on_existing_contacts
+    self.org.contacts.each do |contact|
+      ContactValue.create(contact: contact, contact_property: self)
+    end
+  end
+
 end
