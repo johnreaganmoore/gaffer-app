e9f40df1b397dcb017bcfd08884fe5f1d473a57d
johnreaganmoore <johnreaganmoore@gmail.com>
2017-06-20 09:33:18 -0400

    Add contact_properties endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :contacts, only: [:index, :create, :show, :update, :destroy]
+      resources :contact_properties, only: [:index, :create, :show, :update, :destroy]
       # resources :microposts, only: [:index, :create, :show, :update, :destroy]
     end
   end
