af0ed427bc4834307a542d287ea68840a3f0efc9
johnreaganmoore <johnreaganmoore@gmail.com>
2017-06-28 10:19:15 -0400

    Try another serialization format

--- app/serializers/api/v1/submission_serializer.rb
+++ app/serializers/api/v1/submission_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::SubmissionSerializer < ActiveModel::Serializer
-  attributes :id, :email, :phone, :first_name, :last_name, :status, :created_at, :updated_at, :submission_values
+  attributes :id, :email, :phone, :first_name, :last_name, :status, :created_at, :updated_at, :submission_values, :sub_values
 
   def created_at
     object.created_at.in_time_zone.iso8601 if object.created_at
@@ -8,4 +8,18 @@ class Api::V1::SubmissionSerializer < ActiveModel::Serializer
   def updated_at
     object.updated_at.in_time_zone.iso8601 if object.created_at
   end
+
+
+  def sub_values
+
+    values = []
+
+    object.submission_values.each do |sv|
+      values << sv.display_value
+    end
+
+    return values
+
+  end
+
 end
